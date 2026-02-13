d2feb779d196e439e7a5fc041f5acff833fad46d
Emrah Kinay <emrahkinay@gmail.com>
2026-02-13 16:58:17 -0500

    removed server proxy

--- vite.config.ts
+++ vite.config.ts
@@ -6,16 +6,6 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": {
-        target: process.env.VITE_API_BASE_URL,
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, "/apps/active/api/v3"),
-      },
-    },
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
